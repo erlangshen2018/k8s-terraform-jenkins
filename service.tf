41dd490016090548e0f6401945a4406af2a96665
erlangshen2018 <39119847+erlangshen2018@users.noreply.github.com>
2025-07-05 12:51:27 +0800

    Create service.tf

---
+++ service.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
